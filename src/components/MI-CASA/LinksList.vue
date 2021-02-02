641ec015235e132f9186fef5da144da386f0142e
Juan Castaneda <juavauj@gmail.com>
2021-02-02 15:40:02 -0500

    LINKLIST COMPONENT Update
    - Updating styles
    - Creating mounted animation

--- src/components/MI-CASA/LinksList.vue
+++ src/components/MI-CASA/LinksList.vue
@@ -1,25 +1,29 @@
 <template>
-  <div class="link">
-    <template v-for="item in links" :key="item">
-      <!-- <div class="link__item">{{ item }}</div> -->
-      <router-link
-        class="link__item"
-        :to="item.toString()"
-        v-if="!isExternal"
-        @mouseenter="setTitle(item.toString())"
-      ></router-link>
-      <a
-        :href="item.toString()"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="link__item"
-        v-if="isExternal"
-      ></a>
-    </template>
-  </div>
+  <transition v-on:leave="leave" v-bind:css="false">
+    <div class="link">
+      <template v-for="item in links" :key="item">
+        <!-- <div class="link__item">{{ item }}</div> -->
+        <router-link
+          class="link__item"
+          :to="item.toString()"
+          v-if="!isExternal"
+          @mouseenter="setTitle(item.toString())"
+        ></router-link>
+        <a
+          :href="item.toString()"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="link__item"
+          v-if="isExternal"
+        ></a>
+      </template>
+    </div>
+  </transition>
 </template>
 
 <script>
+import { gsap } from "gsap";
+
 export default {
   props: {
     links: Array,
@@ -30,18 +34,23 @@ export default {
   data() {
     return {
       isExternal: this.external ? true : false,
+      items: null,
     };
   },
   mounted() {
-    let items = this.$el.querySelectorAll(".link__item");
-    this.setSize(items);
-    this.setColor(items);
+    this.items = this.$el.querySelectorAll(".link__item");
+    this.setSize(this.items);
+    this.setColor(this.items);
   },
   methods: {
     setSize(items) {
-      items.forEach((elem) => {
-        elem.style.width = `${this.size ? this.size : 20}px`;
-        elem.style.height = `${this.size ? this.size : 20}px`;
+      items.forEach((elem, index) => {
+        gsap.to(elem, {
+          duration: 0.5,
+          width: "15px",
+          height: "15px",
+          delay: index * 0.5,
+        });
       });
     },
     setColor(items) {
@@ -53,8 +62,25 @@ export default {
       });
     },
     setTitle(title) {
-      this.$emit('changeTitle', title)
-    }
+      this.$emit("changeTitle", title);
+    },
+    leave:function(done) {
+      this.exitAnimation(done);
+      console.log('Se fue');
+    },
+    exitAnimation(done) {
+      this.items.forEach((elem, index) => {
+        gsap.to(elem, {
+          duration: 0.5,
+          width: 0,
+          height: 0,
+          delay: index * 0.5,
+          onComplete: function() {
+            done();
+          },
+        });
+      });
+    },
   },
 };
 </script>
@@ -62,11 +88,11 @@ export default {
 <style lang="scss" scoped>
 @import "./../../assets/styles/setup";
 
-.link { 
+.link {
   //* for this vue
   position: absolute;
   top: 50%;
-  left: 50%; 
+  left: 50%;
   @include transform(translate(-50%, -50%));
   //* end for this vue
 
@@ -76,17 +102,19 @@ export default {
   justify-content: space-between;
 
   .link__item {
-    width: 15px;
-    height: 15px;
+    width: 0;
+    height: 0;
     border-radius: 50%;
-    border: 3px solid #fff;
+    border: 1px solid #fff;
     //   margin: 0 2rem;
-    @include transition(all 0.3s);
+    @include transition(all 0.6s ease-out);
 
     cursor: pointer;
 
     &:hover {
       background-color: #fff;
+      width: 25px !important;
+      height: 25px !important;
     }
   }
 }
