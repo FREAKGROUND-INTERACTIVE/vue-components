2f9efadf18bee01275c8f75b86e81ebfea4db89f
Juan Castaneda <juavauj@gmail.com>
2021-02-02 09:48:51 -0500

    CLIENTLIST COMPONENT Created

---
+++ src/components/MI-CASA/ClientList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="clientList">
+    <div v-for="client in list" :key="client">
+      <img :src="client" alt="Clients" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./../../assets/styles/setup";
+
+.clientList {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  @include transform(translate(-50%, -50%));
+  display: grid;
+  grid-template-columns: auto auto auto auto auto;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: grey;
+    width: 170px;
+    height: 85px;
+    margin: 0.1rem;
+
+    img {
+      width: 75%;
+      height: 75%;
+    }
+  }
+}
+</style>
